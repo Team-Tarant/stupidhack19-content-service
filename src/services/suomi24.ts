7e6701c06b03cdcab437812ab4e575232e1a11f4
Hugo Holmqvist <hugo.holm@live.com>
2019-05-17 21:10:13 +0300

    Fix encoding

--- src/services/suomi24.ts
+++ src/services/suomi24.ts
@@ -4,12 +4,11 @@ import { translateString } from './translateService'
 
 export const getLatestThreads = async () => {
   const randN = Math.floor(Math.random() * 30)
-  console.log(randN)
   const { data } = await axios.get(`https://keskustelu.suomi24.fi/?page=${randN}`)
   const dom = cheerio.load(data)
   const res = dom('.thread-list-item-title').toArray().map(elem => elem.childNodes[0].nodeValue)
   const promises = res
-    .filter(text => text.endsWith('?'))
+    .filter(text => text.endsWith('?') || text.endsWith('!'))
     .map(text => translateString(text))
 
   return Promise.all(promises)
--- src/services/translateService.ts
+++ src/services/translateService.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { text } from 'body-parser';
 
 function createUrl(textToTranslate: string) {
-  return `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${process.env.YANDEX_API_KEY}&text=${textToTranslate}&lang=fi-en`
+  return `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${process.env.YANDEX_API_KEY}&text=${encodeURIComponent(textToTranslate)}&lang=fi-en`
 }
 
 export const translateString = (string: string) =>
